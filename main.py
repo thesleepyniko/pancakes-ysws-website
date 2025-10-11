ed7d1f2d283983278b3405862a6d1c0d5381e31c
Niko <137782638+thesleepyniko@users.noreply.github.com>
2025-10-11 15:42:05 -0400

    Begin work on website, add html, add css, create fastAPI project

---
+++ main.py
@@ -0,0 +1,15 @@
+# this code is going to serve the static files and thats probably all, maybe like an api endpoint or two
+
+from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+
+app = FastAPI()
+
+# app.mount("/static", StaticFiles(directory="./static"), name="static")
+
+# @app.get("/")
+# def read_root():
+#     return {"Hello": "World"}
+
+# mounting static directory, telling fastAPI it's html, and serving it at /
+app.mount("/", StaticFiles(directory="static", html=True), name="frontend") 
